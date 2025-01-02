d8cee1491d74dfefe40087b3a5dae1d11ece9592
Ali Sasani <a_S47_1@yahoo.com>
2025-01-02 15:12:40 +0330

    [init] 🚀 init module

---
+++ app/Providers/BaseServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Base\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class BaseServiceProvider extends ServiceProvider
+{
+	public function register()
+	{
+	}
+	
+	public function boot()
+	{
+	}
+}
---
+++ tests/BaseServiceProviderTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Modules\Base\Tests;
+
+class BaseServiceProviderTest extends \Tests\TestCase
+{
+	// TODO
+}
