926a51af934cc254956abbd0156f8c898683d79b
Ali Sasani <a_S47_1@yahoo.com>
2025-06-27 18:52:38 +0330

    [feat] add livewire and filamentModule config file

--- app/Providers/BaseServiceProvider.php
+++ app/Providers/BaseServiceProvider.php
@@ -2,15 +2,36 @@
 
 namespace Modules\Base\Providers;
 
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\ServiceProvider;
 
 class BaseServiceProvider extends ServiceProvider
 {
-	public function register()
-	{
-	}
-	
-	public function boot()
-	{
-	}
+    public function register()
+    {
+        $this->registerConfig();
+
+    }
+
+    public function boot()
+    {
+        //
+    }
+
+    private function registerConfig(): void
+    {
+        foreach (File::glob(__DIR__ . '/../../config/*.php') as $file) {
+            if (basename($file) == 'graphql.php') {
+                continue;
+            }
+
+            $this->publishes([
+                $file => config_path(basename($file)),
+            ], 'config');
+
+            $this->mergeConfigFrom(
+                $file, basename($file, '.php')
+            );
+        }
+    }
 }
